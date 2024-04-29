bef76dec8e7dcca88a49b49162711f4b151bf64c
Moïra <moira.vanderslagmolen@ulb.be>
2024-04-29 23:08:13 +0200

    retrieve info from the database

--- website/index.php
+++ website/index.php
@@ -17,12 +17,19 @@ try {
   $conn = new PDO("mysql:host=$servername", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE myDBPDO";
+  $sql = file_get_contents("database/init.sql");
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "Database created successfully<br>";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
+}
+
+$sql = "SELECT * FROM test_table";
+$stmt = $conn->query($sql);
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo $row['name'] . "<br>";
 }
 
 $conn = null;
