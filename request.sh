9b0f555d1b26481b4b477160783ea94758f30865
Moïra <moira.vanderslagmolen@ulb.be>
2024-05-01 18:31:52 +0200

    lb works

---
+++ request.sh
@@ -0,0 +1,4 @@
+sudo docker-compose down
+sudo docker-compose up --build
+
+for i in {1..10}; do curl localhost:8082; done
--- src/views/welcome.php
+++ src/views/welcome.php
@@ -1,4 +1,4 @@
 <?php
-echo "Welcome page";
+echo "Welcome page to server 1";
 
 ?>
