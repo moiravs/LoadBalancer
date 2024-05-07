16a8353398563aa4fa81cc6bb7a8bbdc93eeca34
Moïra <moira.vanderslagmolen@ulb.be>
2024-05-07 09:15:18 +0200

    change by ip adress

--- src/views/account.php
+++ src/views/account.php
@@ -4,9 +4,5 @@ require_once dirname(__DIR__) . '/database/database.php';
 echo "Account";
 echo 'Server: ' . gethostname();
 
-$sql = "SELECT * FROM test_table";
-$stmt = $conn->query($sql);
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo $row['id'] . "<br>";
-}
+
 ?>
